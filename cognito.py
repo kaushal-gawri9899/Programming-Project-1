4797583ad4ec7109569f31fe43c7be1b9aeeaf10
Muhammad Tariq <62224301+alitariqs3779826@users.noreply.github.com>
2021-04-22 22:08:15 +1000

    Job Postings Table and File upload updated

--- cognito.py
+++ cognito.py
@@ -14,11 +14,11 @@ from tika import parser
 import json
 
 from pyresparser import ResumeParser
-# from gensim.summarization.summarizer import summarize
-# from gensim.summarization import keywords# Import the library
-# from pdfminer.high_level import extract_text
-# from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.metrics.pairwise import cosine_similarity
+from gensim.summarization.summarizer import summarize
+from gensim.summarization import keywords# Import the library
+from pdfminer.high_level import extract_text
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import docx2txt
 import uuid
 
@@ -89,13 +89,13 @@ def signup():
 @cognitoRoute.route('/auth/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        # global stored_email
+        global stored_email
         data = request.get_json()
         print(data)
         user_email = data['email']
         password = data['password']
         # return jsonify({ "userType": "Employer", "idToken": ""})
-        # stored_email = user_email
+        stored_email = user_email
         
        
 
@@ -136,45 +136,6 @@ def login():
         return jsonify({ 'userType': usertype, 'idToken': id_token})        
         
     return 'a'
-
-
-@cognitoRoute.route('/upload', methods=['GET','POST'])
-def fileUpload():
-    target=os.path.join(UPLOAD_FOLDER,'resume_saved')
-    
-    
-
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    
-    file = request.files['file']
-    
-    if file: 
-        filename = secure_filename(stored_email)
-        destination="/".join([target, filename])
-        file.save(destination)
-        # session['uploadFilePath']=destination
-        uploaded_file_name = 'resume_saved/' + filename
-        
-        s3_client.upload_file(
-                    Bucket = BUCKET_NAME,
-                    Filename = uploaded_file_name,
-                    Key = filename
-                )
-
-    
-    data = ResumeParser(uploaded_file_name).get_extracted_data()
-    name = data['name']
-    experience = data['experience']
-    
-    r = requests.post("https://kor6ktyjri.execute-api.us-east-1.amazonaws.com/dev/resume", 
-        json= {"email":stored_email,"experience":str(experience),"name":str(name)}) 
-    
-    # print(data['name'])
-    
-        
-    response = " "
-    return response
 
 @cognitoRoute.route('/match', methods=['GET','POST'])
 def job_match():
@@ -240,68 +201,98 @@ def postPosting():
 
 @cognitoRoute.route('/jobs', methods=['GET','POST'])
 def getJobs():
-    print('ss')
-    # print(request.get_json())
     headers = request.headers.get('Authorization')
-    # print(request.headers.get('Authorization'))
-    # print(request.headers)
-    # dynamodb = boto3.resource('dynamodb',  region_name='us-east-1')
-    # table = dynamodb.Table('Job_Postings')
-    # response = table.query(
-    #         KeyConditionExpression=Key('email').eq('prabhav@mehra.com')
-    # )
-    # print(response)
-    # dynamodb_client = boto3.client('dynamodb', region_name="us-east-1")
-    # response = dynamodb_client.scan(
-    #     # FilterExpression=Attr('email').eq("prabhav@mehra.com"),
-        
-    #     ExpressionAttributeNames={
-    #         '#JT': 'jobType',
-    #         '#JD': 'jobDescription',
-    #         '#L': 'location',
-    #         '#WE': 'workExperience',
-    #         '#JI': 'jobTitle',
-            
-    #     },
-    #     ExpressionAttributeValues={
-    #         ':a': {
-    #             'S': 'prabhav@mehra.com',
-    #         },
-    #     },
-    #     FilterExpression='email = :a',
-    #     ProjectionExpression='#JT,#JD,#L,#WE,#JI',
-    #     TableName='Job_Postings',
-    # )
-    # print(response)
+   
     r = requests.get('https://jypfk3zpod.execute-api.us-east-1.amazonaws.com/dev/jobs',
     headers={"Authorization": headers}
     )
-    # 
-    # print(r.json()['Items'])
-    # print(number_of_elements)
-    # dynamodb = boto3.resource('dynamodb',  region_name='us-east-1')
+   
+    data = r.json()
+    number_of_elements = int(r.json()['Count'])
+    
+    jobTitle = []
+    jobType = []
+    location = []
+    workExperience = []
+    jobDescription = []
+
+    for i in range(number_of_elements):
+        jobType.append(r.json()['Items'][i]['jobType']['S'])
+        location.append(r.json()['Items'][i]['location']['S'])
+        jobDescription.append(r.json()['Items'][i]['jobDescription']['S'])
+        workExperience.append(r.json()['Items'][i]['workExperince']['S'])
+        jobTitle.append(r.json()['Items'][i]['jobTitle']['S'])
 
     
-    # table = dynamodb.Table('Job_posting')
-    # response = table.query(
-    #     KeyConditionExpression=Key('email').eq("test@test.com")
-    # )
-    # print(response['Items'])
-    data = r.json()
-    new_d = json.loads(r)
-    items = data['Items']
-    print(new_d)
-    # print(readable_json[0]['firstName']])
-    count = data['Count']
-    # print(data['Items']['jobType'])
-    # json = ' '
-    # for i in range(count):
-    #     print(i)
-    #     json = 'Items'
-    # print(count)
-    # print(data)
-   
-    # print(session['test'])
-    # print(data)
+
+    returnedJson  = {"Items":[{"jobDescription": jobDescription[0], "jobType": jobType[0], "location": location[0], "jobTitle": jobTitle[0], "workExperince"
+                : workExperience[0]}]}
+
+    storingJson = returnedJson['Items']
+
+    for i in range(number_of_elements):
+        if i > 0:
+            updatedJson  = {"jobDescription": jobDescription[i], "jobType": jobType[i], "location": location[i], "jobTitle": jobTitle[i], "workExperince"
+                    : workExperience[i]}
+            storingJson.append(updatedJson)
     
-    return data
+    finalJson = json.dumps(storingJson)
+    print(finalJson)
+
+
+    return finalJson
+
+@cognitoRoute.route('/match', methods=['GET','POST'])
+def getMatcheddata():
+    return 'nothing'
+
+@cognitoRoute.route('/experience', methods=['GET','POST'])
+def getExperience():
+    headers = request.headers.get('Authorization')
+
+    r = requests.get('https://kor6ktyjri.execute-api.us-east-1.amazonaws.com/dev/experience',
+    headers={"Authorization": headers})
+    
+    data = r.json()
+    print(data)
+    return 'nothing'
+
+# Need to change Stored Email, Stored Email is Global for now
+@cognitoRoute.route('/upload', methods=['GET','POST'])
+def fileUpload():
+    headers = request.headers.get('Authorization')
+
+    target=os.path.join(UPLOAD_FOLDER,'resume_saved')
+    
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    
+    file = request.files['file']
+    
+    if file: 
+        filename = secure_filename(file.filename)
+        destination="/".join([target, filename])
+        file.save(destination)
+        session['uploadFilePath']=destination
+        uploaded_file_name = 'resume_saved/' + file.filename
+        
+        s3_client.upload_file(
+                    Bucket = BUCKET_NAME,
+                    Filename = uploaded_file_name,
+                    Key = filename
+                )
+
+    
+    data = ResumeParser(uploaded_file_name).get_extracted_data()
+    name = data['name']
+    experience = data['total_experience']
+    
+    r = requests.post("https://kor6ktyjri.execute-api.us-east-1.amazonaws.com/dev/resume", 
+        headers={"Authorization": headers},
+        json= {"email":stored_email,"experience":str(experience),"name":str(name)}) 
+    
+   
+    print(data)
+        
+    response = " "
+    return response
--- pared.py
+++
@@ -1,5 +0,0 @@
-
-
-from pyresparser import ResumeParser
-data = ResumeParser('resume.pdf').get_extracted_data()
-print(data)
